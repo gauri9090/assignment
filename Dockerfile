1d52feac5fbc7250ceaf275cf2395c31aa310ac6
Gauri Raghuwanshi <132771730+gauri9090@users.noreply.github.com>
2024-03-31 06:17:10 -0400

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-#Use a base image 
-FROM alpine:latest
+FROM ubuntu:latest
 
+
+RUN apt-get -y update && apt-get -y install curl
 #Copy the script into the container
-COPY /home/graghuva/Gauri/getMetricsScript.sh /getMetricsScript.sh
+COPY Gauri/getMetricsScript.sh /root/getMetricsScript.sh
 
 #Grant executable permissions to the script
-RUN chmod +x /getMetricsScript.sh
+RUN chmod +x /root/getMetricsScript.sh
 
 #Command to execute the script
-CMD ["./getMetricsScript.sh"]
+
+CMD ["/bin/sh", "-c", "/root/getMetricsScript.sh && sleep 1200"]
+
--- getMetricsScript.sh
+++ getMetricsScript.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-RESULT_DIR="/myNodeMetrics"
+RESULT_DIR="/metrics"
 
 mkdir -p $RESULT_DIR
 
 CURRENT_TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
-METRICS_CMD="node_exporter &"
+METRICS_CMD="curl -s http://node-exporter.default.svc.cluster.local:9100/metrics "
 
 METRIC_FILE="$RESULT_DIR/metricsAT_$CURRENT_TIMESTAMP.txt"
 
-$METRICS_CMD 
-
-curl -s http://localhost:9100/metrics | grep -E '^node_(cpu_seconds_total|memory_(usage|bytes))|node_filesystem_usage' > $METRIC_FILE
+$METRICS_CMD  > $METRIC_FILE
 
 echo "Metric file is created and stored here: $METRIC_FILE"
 
